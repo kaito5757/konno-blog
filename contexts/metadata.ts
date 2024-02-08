8593054d8cd7967ed6402f3ebf5d250fac2f36e6
konno <kaito.121855@gmail.com>
2024-02-08 22:37:05 +0900

    update: metadataの更新

---
+++ contexts/metadata.ts
@@ -0,0 +1,7 @@
+export const siteMetaData = {
+  siteUrl: "https://study-blog.vercel.app",
+  title: "study-blog",
+  description: "学習用のブログサイト",
+  ogpType: "website",
+  ogpImage: "https://placehold.jp/1200x630.png",
+};

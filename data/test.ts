90941c6d8108aba04f8ed2a13bd0a37c143ba3da
konno <kaito.121855@gmail.com>
2024-10-29 09:46:13 +0900

    update: 「ユーザー更新処理」の追加

--- data/test.ts
+++ data/test.ts
@@ -25,17 +25,41 @@ export class UserName {
   }
 }
 
+export class MailAddress {
+  private mailAddress: string
+
+  constructor(mailAddress: string) {
+    if (mailAddress.length < 3) {
+      throw new Error('ユーザー名は3文字以上です。')
+    }
+    if (mailAddress.length > 20) {
+      throw new Error('メールアドレスは20文字以下です。')
+    }
+
+    this.mailAddress = mailAddress
+  }
+
+  get value(): string {
+    return this.mailAddress
+  }
+}
+
 export class User {
   private readonly userId: UserId
   private userName: UserName
-
-  constructor(userName: UserName) {
+  private userMailAddress: MailAddress
+  constructor(userName: UserName, userMailAddress: MailAddress) {
     this.userId = new UserId(Math.random().toString(36).substring(2, 10))
     this.userName = userName
+    this.userMailAddress = userMailAddress
   }
 
   public changeName(userName: UserName): void {
     this.userName = userName
+  }
+
+  public changeMailAddress(userMailAddress: MailAddress): void {
+    this.userMailAddress = userMailAddress
   }
 
   get id(): UserId {
@@ -44,6 +68,10 @@ export class User {
 
   get name(): UserName {
     return this.userName
+  }
+
+  get mailAddress(): MailAddress {
+    return this.userMailAddress
   }
 }
 
@@ -55,22 +83,30 @@ export interface IUserRepository {
 }
 
 export class UserRepository implements IUserRepository {
-  private readonly userDataStore: { userId: string; userName: string }[]
+  private readonly userDataStore: { userId: string; userName: string; userMailAddress: string }[]
 
   public findById(userId: UserId): User | null {
     const user = this.userDataStore.find((user) => user.userId === userId.value)
-    return user ? new User(new UserName(user.userName)) : null
+    return user
+      ? new User(new UserName(user.userName), new MailAddress(user.userMailAddress))
+      : null
   }
 
   public findByName(userName: UserName): User | null {
     const user = this.userDataStore.find((user) => user.userName === userName.value)
-    return user ? new User(new UserName(user.userName)) : null
+    return user
+      ? new User(new UserName(user.userName), new MailAddress(user.userMailAddress))
+      : null
   }
 
   public save(user: User): void {
     const index = this.userDataStore.findIndex((userData) => userData.userId === user.id.value)
     if (index === -1) {
-      this.userDataStore.push({ userId: user.id.value, userName: user.name.value })
+      this.userDataStore.push({
+        userId: user.id.value,
+        userName: user.name.value,
+        userMailAddress: user.mailAddress.value,
+      })
     } else {
       this.userDataStore[index].userName = user.name.value
     }
@@ -97,8 +133,8 @@ export class UserApplicationService {
   ) {}
 
   // ユーザー登録処理
-  public register(name: string): void {
-    const user = new User(new UserName(name))
+  public register(name: string, mailAddress: string): void {
+    const user = new User(new UserName(name), new MailAddress(mailAddress))
     if (this.userService.exists(user)) {
       throw new Error('ユーザーが既に存在します。')
     }
@@ -114,6 +150,21 @@ export class UserApplicationService {
     }
     return new UserDto(user)
   }
+
+  // ユーザー更新処理
+  public update(command: UserUpdateCommand): void {
+    const user = this.userRepository.findById(new UserId(command.id))
+
+    if (!user) {
+      throw new Error('ユーザーが存在しません。')
+    }
+    user.changeName(new UserName(command.name))
+
+    if (command.mailAddress) {
+      user.changeMailAddress(new MailAddress(command.mailAddress))
+    }
+    this.userRepository.save(user)
+  }
 }
 
 class Client {
@@ -127,5 +178,29 @@ export class UserDto {
   constructor(user: User) {
     this.id = user.id.value
     this.name = user.name.value
+  }
+}
+
+export class UserUpdateCommand {
+  private userId: string
+  private userName: string
+  private userMailAddress?: string
+
+  constructor(userId: string, userName: string, userMailAddress?: string) {
+    this.userId = userId
+    this.userName = userName
+    this.userMailAddress = userMailAddress
+  }
+
+  get id(): string {
+    return this.userId
+  }
+
+  get name(): string {
+    return this.userName
+  }
+
+  get mailAddress(): string | undefined {
+    return this.userMailAddress
   }
 }
